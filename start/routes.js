0e6165e07aaa07bc8bb7026029bb46fded8bbf07
douglas-cavalcante <henriquedouglas2803@gmail.com>
2020-09-17 15:12:36 -0300

    Modifica logica do auth

--- start/routes.js
+++ start/routes.js
@@ -2,24 +2,34 @@
 
 const Route = use('Route')
 
-Route.get('/trainees', 'TraineeController.index')
-Route.get('/trainees/:id', 'TraineeController.show')
-Route.post('/trainees', 'TraineeController.store')
-Route.put('/trainees/:id', 'TraineeController.update')
+Route.post('/users', 'UserController.store');
+Route.post('/sessions', 'SessionController.store');
 
-Route.get('/companies', 'CompanyController.index')
-Route.get('/companies/:id', 'CompanyController.show')
-Route.post('/companies', 'CompanyController.store')
-Route.put('/companies/:id', 'CompanyController.update')
+Route.get('/trainees', 'TraineeController.index');
+Route.get('/trainees/:id', 'TraineeController.show');
+Route.post('/trainees', 'TraineeController.store');
+Route.put('/trainees/:id', 'TraineeController.update');
 
-Route.get('/observations', 'ObservationController.index')
-Route.get('/observations/:id', 'ObservationController.show')
-Route.post('/observations', 'ObservationController.store')
-Route.put('/observations/:id', 'ObservationController.update')
+Route.get('/companies', 'CompanyController.index');
+Route.get('/companies/:id', 'CompanyController.show');
+Route.post('/companies', 'CompanyController.store');
+Route.put('/companies/:id', 'CompanyController.update');
+
+Route.get('/observations', 'ObservationController.index');
+Route.get('/observations/:id', 'ObservationController.show');
+Route.post('/observations', 'ObservationController.store');
+Route.put('/observations/:id', 'ObservationController.update');
 
 
 Route.post('/contracts', 'ContractController.store');
 Route.get('/contracts', 'ContractController.index');
 
 Route.put('/contracts/:id/shutdown', 'ContractController.shutdown');
+
+Route.get('/dashboard', 'DashboardController.index');
+
+Route.get('/reports/actives', 'ActiveContractController.index');
+Route.get('/reports/inactives', 'InactiveContractController.index');
+
+
 
