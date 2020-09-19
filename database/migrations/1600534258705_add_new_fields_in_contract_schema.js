7aab44b69f7bb160d6def460549ed2554c959685
douglas-cavalcante <henriquedouglas2803@gmail.com>
2020-09-19 14:03:50 -0300

    Corrige migraton

--- database/migrations/1600534258705_add_new_fields_in_contract_schema.js
+++ database/migrations/1600534258705_add_new_fields_in_contract_schema.js
@@ -5,14 +5,14 @@ const Schema = use('Schema')
 
 class AddNewFieldsInContractSchema extends Schema {
   up () {
-    this.table('users', (table) => {
+    this.table('contracts', (table) => {
       table.float('integration_agent_value')
       table.float('institution_value_company')
     })
   }
 
   down () {
-    this.table('users', (table) => {
+    this.table('contracts', (table) => {
       // reverse alternations
     })
   }
