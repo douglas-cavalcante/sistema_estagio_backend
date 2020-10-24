57fffef87f484a5c78e4e497a114965ee5c89aa9
douglas-cavalcante <henriquedouglas2803@gmail.com>
2020-10-24 19:00:06 -0300

    adiciona retorno idade do estagiario na listagem

--- app/Models/Trainee.js
+++ app/Models/Trainee.js
@@ -1,9 +1,19 @@
 'use strict'
 
 const Model = use('Model')
+const {differenceInYears} = require('date-fns');
 
 class Trainee extends Model {
 
+  static get computed () {
+    return ['age']
+  }
+
+  getAge ({ date_birth }) {
+    return Number(`${differenceInYears(new Date(), date_birth)}`);
+  }
+
+ 
   contract () {
     return this.hasOne('App/Models/Contract')
   }
